3a6cbc5ab558099ec1d5f45e83f2fcd5adf66bcf
Babatunde Quadre <quadremustapha@gmail.com>
2021-02-08 18:25:00 -0500

    Openining hours conversion

---
+++ src/Services/IOpeningHoursService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using OpeningHours.API.Models;
+
+namespace OpeningHours.API.Services
+{
+    public interface IOpeningHoursService
+    {
+        Response ProcessRequest(DayOfWeekInputDto inputDto);
+    }
+}
